7af92d008b0d4f3d215c8e44af0532d1b50b5493
ArnaldOzPy <156711582+ArnaldOzPy@users.noreply.github.com>
2025-07-16 11:38:44 -0300

    Update decompress-worker.js

--- workers/decompress-worker.js
+++ workers/decompress-worker.js
@@ -1,13 +1,13 @@
 import { inverse6D } from '../bwt-rle.js';
 import { decompressHS6D } from '../hyper-huffman.js';
 
-self.onmessage = async (e) => {
+self.onmessage = (e) => {
   const compressedData = e.data;
-  
+
   try {
     const huffmanDecompressed = decompressHS6D(compressedData);
     const originalData = inverse6D(huffmanDecompressed);
-    
+
     self.postMessage({
       decompressed: originalData,
       compressedSize: compressedData.length
