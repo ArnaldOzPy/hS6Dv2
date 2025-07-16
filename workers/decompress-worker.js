56e0ab9f9a4fcae620f203f8b5c778ab67e9fdd1
ArnaldOzPy <156711582+ArnaldOzPy@users.noreply.github.com>
2025-07-16 13:28:09 -0300

    Update decompress-worker.js

--- workers/decompress-worker.js
+++ workers/decompress-worker.js
@@ -1,13 +1,17 @@
 import { inverse6D } from '../bwt-rle.js';
 import { decompressHS6D } from '../hyper-huffman.js';
 
-self.onmessage = (e) => {
-  const compressedData = e.data;
-
+self.onmessage = async (e) => {
   try {
+    const compressedData = e.data;
+    
+    // Descomprimir Huffman
     const huffmanDecompressed = decompressHS6D(compressedData);
+
+    // Inversión de BWT/MTF
     const originalData = inverse6D(huffmanDecompressed);
 
+    // Enviar resultado
     self.postMessage({
       decompressed: originalData,
       compressedSize: compressedData.length
