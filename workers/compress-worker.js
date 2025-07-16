ca19d1a7adcc06a9c38c31a74f16c0025dd78244
ArnaldOzPy <156711582+ArnaldOzPy@users.noreply.github.com>
2025-07-16 09:22:58 -0300

    Create compress-worker.js

---
+++ workers/compress-worker.js
@@ -0,0 +1,20 @@
+import { transform6D } from '../bwt-rle.js';
+import { compressHS6D } from '../hyper-huffman.js';
+
+self.onmessage = async (e) => {
+  const data = e.data;
+  
+  try {
+    const bwtCompressed = transform6D(data);
+    const huffmanCompressed = compressHS6D(bwtCompressed);
+    
+    self.postMessage({
+      compressed: huffmanCompressed.compressed,
+      originalSize: data.length,
+      compressedSize: huffmanCompressed.compressed.length
+    }, [huffmanCompressed.compressed.buffer]);
+    
+  } catch (error) {
+    self.postMessage({ error: error.message });
+  }
+};
