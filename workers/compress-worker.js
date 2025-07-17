9b018f7e95a7a52cce7511804bfa3afce63087f7
ArnaldOzPy <156711582+ArnaldOzPy@users.noreply.github.com>
2025-07-17 10:03:29 -0300

    Update compress-worker.js

--- workers/compress-worker.js
+++ workers/compress-worker.js
@@ -1,18 +1,39 @@
 import { transform6D } from '../bwt-rle.js';
 import { compressHS6D } from '../hyper-huffman.js';
 
+
+const isBinaryData = (data) => {
+  let entropy = 0;
+  const freq = new Array(256).fill(0);
+  for (const byte of data) freq[byte]++;
+  for (const count of freq) {
+    if (count > 0) {
+      const p = count / data.length;
+      entropy -= p * Math.log2(p);
+    }
+  }
+  return entropy > 7.9; 
+};
+
 self.onmessage = (e) => {
   const data = e.data;
 
   try {
-    const bwtCompressed = transform6D(data);
-    const huffmanCompressed = compressHS6D(bwtCompressed);
+    let compressedData;
+    
+    if (isBinaryData(data)) {
+      
+      compressedData = compressHS6D(data).compressed;
+    } else {
+      const bwtCompressed = transform6D(data);
+      compressedData = compressHS6D(bwtCompressed).compressed;
+    }
 
     self.postMessage({
-      compressed: huffmanCompressed.compressed,
+      compressed: compressedData,
       originalSize: data.length,
-      compressedSize: huffmanCompressed.compressed.length
-    }, [huffmanCompressed.compressed.buffer]);
+      compressedSize: compressedData.length
+    }, [compressedData.buffer]);
     
   } catch (error) {
     self.postMessage({ error: error.message });
