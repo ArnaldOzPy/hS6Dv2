f14d376769cf62c302fd69ac8e9378377f92f192
ArnaldOzPy <156711582+ArnaldOzPy@users.noreply.github.com>
2025-07-26 20:45:02 -0300

    Update compress-worker.js

--- workers/compress-worker.js
+++ workers/compress-worker.js
@@ -140,6 +140,11 @@ self.onmessage = async (e) => {
 
     reportProgress(1.0, 'Finalizado');
 
+    // Liberar memoria de datos intermedios
+    if (bwtData && bwtData !== data) {
+        bwtData = null; // Ayuda al recolector de basura
+    }
+
     self.postMessage({
       compressed: finalOutput,
       originalSize: data.length,
